43f07eb17e99f3bdfbe4a615a5a2b76c6eb7d42f
dwilestarilestari <dwilestaritarii03@gmail.com>
2019-10-19 07:57:51 +0700

    Initial commit

---
+++ app/src/main/java/id/ac/tari/prj01/MainActivity.java
@@ -0,0 +1,26 @@
+package id.ac.tari.prj01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText TextNama;
+    TextView Hasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextNama=(EditText)findViewById(R.id.TxtNama);
+        Hasil = (TextView) findViewById(R.id.Lbl_Nama);
+    }
+
+    public void TampilNama (View v){
+        Hasil.setText("Nama anda: "+TextNama.getText());
+    }
+}
